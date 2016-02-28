044cefa4d5cb274f210687d2ac95b980460de07b
Susan Elliott Sim <ses@drsusansim.org>
2016-02-28 17:48:06 -0500

    Cleaned up code and tests cases from last commit

--- rover.py
+++ rover.py
@@ -25,26 +25,55 @@ __license__ = "MIT License"
 import sys
 import textwrap
 
+
 class Usage(Exception):
     def __init(self, message):
         self.message = message
 
 
-def get_plateau():
+def get_instructions(prompt):
+    """
+    Prompts the user to enter instructions to the rover. Inputs must consist of a sequence of Ls, Rs, and Ms in
+    any order.
+    :return: a list strings, each consisting of one character that is an L, R, or M
+    """
 
     output = []
 
-    input_plateau = raw_input("Plateau:")
-    input_plateau = input_plateau.split()
+    input_string = raw_input(prompt)
+    input_list = input_string.split()
 
-    if len(input_plateau) != 2:
-        raise Usage("Plateau must have two coordinates")
+    if len(input_list) != 2:
+        raise Usage("Coordinates must have an x and a y value")
 
-    for number in input_plateau:
+    for number in input_list:
         if number.isdigit():
             output.append(int(number))
         else:
-            raise Usage("Plateau coordinates must be positive integers")
+            raise Usage("Coordinates must be positive integers")
+
+    return output
+
+def get_location(prompt):
+    """
+    Prompts the user to enter coordinates. They must be positive integers
+    separated by spaces.
+    :return: a list two ints containing the x and y coordinates specifying the plateau
+    """
+
+    output = []
+
+    input_string = raw_input(prompt)
+    input_list = input_string.split()
+
+    if len(input_list) != 2:
+        raise Usage("Coordinates must have an x and a y value")
+
+    for number in input_list:
+        if number.isdigit():
+            output.append(int(number))
+        else:
+            raise Usage("Coordinates must be positive integers")
 
     return output
 
@@ -54,12 +83,16 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
     # parse command line options
-    if len(argv) == 1:
-        plateau = get_plateau()
-    elif argv[1] == "-h" or argv[1] == "--help":
-        print (textwrap.dedent(__doc__))
+
+    if len(argv) > 1:
+        if argv[1] == "-h" or argv[1] == "--help":
+            print (textwrap.dedent(__doc__))
+        else:
+            print "Invalid syntax: " + "".join((" "+s) for s in argv)
     else:
-        print "Invalid syntax: " + "".join((" "+s) for s in argv)
+        plateau = get_location("Plateau:")
+        landing = get_location("Rover1 Landing:")
+        instructions = get_instructions("Rover1 Instructions:")
 
 if __name__ == "__main__":
     main()
--- test_rover_inputs.py
+++ test_rover_inputs.py
@@ -9,7 +9,7 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2016 Susan Sim"
 __license__ = "MIT License"
 
-from rover import main, Usage, get_plateau
+from rover import main, Usage, get_location
 import mock
 
 
@@ -43,7 +43,6 @@ def test_cli_error(capsys):
     """Tests calling main with invalid arguments
     """
     options = ["k", "--l", "-1"]
-
     for opt in options:
         # Create argument list
         args = ["rover.py", opt]
@@ -71,13 +70,16 @@ def test_cli_plateau(capsys):
             else:
                 assert False
 
+
 def test_get_plateau_error():
+    """Tests calling inputting invalid coordinates to get_plateau()
+    """
     top_corners=["a", "a b", "a, b", "1", "1,2", "1, 2", "-1 -3"]
     # Call function:
     for corner in top_corners:
         with mock.patch("__builtin__.raw_input", return_value=corner):
             try:
-                get_plateau()
+                get_location("Location:")
             except Usage:
                 assert True
             else:
@@ -85,12 +87,16 @@ def test_get_plateau_error():
 
 
 def test_get_plateau_correct():
+    """Tests calling inputting valid coordinates to get_plateau()
+    """
     top_corners=["1 1", "2 5", "9223372036854775807 9223372036854775807"]
+    xy_list = [[1, 1], [2, 5], [9223372036854775807, 9223372036854775807]]
     # Call function:
-    for corner in top_corners:
+    for corner, xy in zip(top_corners, xy_list):
         with mock.patch("__builtin__.raw_input", return_value=corner):
             try:
-                xy = get_plateau()
+                input_loc = get_location("Location:")
+                assert input_loc == xy
             except Usage as u:
                 print u.message
                 assert False
