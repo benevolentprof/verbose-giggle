0514c7a6e9d0c12ed3262cf2f0bd04e18115d278
Susan Elliott Sim <ses@drsusansim.org>
2016-03-06 19:16:39 -0500

    Moved exceptions into their own file

--- exceptions.py
+++ exceptions.py
@@ -1,9 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-""" Docstring """
+""" Exceptions used by  """
 
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2016 Susan Sim"
 __license__ = "MIT License"
 
+
+class Usage(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class InternalError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class ImproperInstruction(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class ImproperPlateau(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class ImproperLanding(Exception):
+    def __init__(self, message):
+        self.message = message
